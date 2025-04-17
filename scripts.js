d252c2318d2e3a04269b37329df568353cabd6f9
dalden2007 <deniegaalden1225@gmail.com>
2025-04-17 16:45:06 -0400

    a

--- scripts.js
+++ scripts.js
@@ -67,6 +67,7 @@ window.addEventListener('scroll', function() {
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
+
     const movePercent = scrollY / window.innerWidth; // optional: normalize it
     const movebooksX = -movePercent * 25 + 10; 
     
